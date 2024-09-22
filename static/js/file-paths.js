4d753b1e99f853efcc898255b26efcefbefac6b1
simran-khanuja <skhanuja@andrew.cmu.edu>
2024-09-22 16:03:32 -0400

    adding results

---
+++ static/js/file-paths.js
@@ -0,0 +1,25 @@
+file_paths = {
+    "dessert-e2e": "./static/images/results/dessert/e2e.png",
+    "dessert-cap-edit": "./static/images/results/dessert/cap-edit.png",
+    "dessert-cap-retrieve": "./static/images/results/dessert/cap-retrieve.png",
+
+    "cotton-e2e": "./static/images/results/cotton/e2e.jpeg",
+    "cotton-cap-edit": "./static/images/results/cotton/cap-edit.png",
+    "cotton-cap-retrieve": "./static/images/results/cotton/cap-retrieve.jpeg",
+
+    "burger-e2e": "./static/images/results/burger/e2e.jpeg",
+    "burger-cap-edit": "./static/images/results/burger/cap-edit.png",
+    "burger-cap-retrieve": "./static/images/results/burger/cap-retrieve.jpeg",
+
+    "ramen-e2e": "./static/images/results/ramen/e2e.jpeg",
+    "ramen-cap-edit": "./static/images/results/ramen/cap-edit.png",
+    "ramen-cap-retrieve": "./static/images/results/ramen/cap-retrieve.jpeg",
+
+    "coke-e2e": "./static/images/results/coke/e2e.png",
+    "coke-cap-edit": "./static/images/results/coke/cap-edit.jpg",
+    "coke-cap-retrieve": "./static/images/results/coke/cap-retrieve.jpeg",
+
+    "currency-e2e": "./static/images/results/currency/e2e.png",
+    "currency-cap-edit": "./static/images/results/currency/cap-edit.png",
+    "currency-cap-retrieve": "./static/images/results/currency/cap-retrieve.png",
+}
